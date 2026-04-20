dc70a178789c863a37b4bbbbecc0c94732510521
acdcmaia <152646700+acdcmaia@users.noreply.github.com>
2026-04-20 14:28:29 -0300

    build: mover variante para o final do nome do APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ tasks.whenTaskAdded {
                     ?.filter { it.name.startsWith("app-") && it.extension == "apk" }
                     ?.forEach { apk ->
                         apk.copyTo(
-                            File(apk.parent, "CallGuard-${android.defaultConfig.versionName}-$variant-$buildDate.apk"),
+                            File(apk.parent, "CallGuard-${android.defaultConfig.versionName}-$buildDate-$variant.apk"),
                             overwrite = true
                         )
                     }
